3e4289fb56dcee2a27e7b035cf6631fe3febd3bf
yasser <spookyframework@gmail.com>
2020-03-18 16:33:17 +0100

    feat: Add env helper function

---
+++ helpers.ts
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the MasterMind project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @desc Gets the value of an environment variable. Supports boolean, empty and null.
+ * @param {string} key
+ * @param { string | number | boolean } value
+ * @returns {string}
+ */
+export function env(key: string, value: string | number | boolean = null) {
+  return process.env[key] || value;
+}
