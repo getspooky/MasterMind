95a830489b1cbf69ce339c31af654011c2c7ae28
yasser <spookyframework@gmail.com>
2020-03-18 14:35:33 +0100

    feat(database): Add User Schema

---
+++ database/schemas/2020_03_18_create_user_schema.ts
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the MasterMind project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Schema } from "mongoose";
+
+/* User Schema */
+export const CreateUserSchema: Schema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: 10
+    },
+    email: {
+      type: String,
+      required: true,
+      minLength: 5,
+      validate: {
+        validator(v) {
+          // eslint-disable-next-line no-useless-escape
+          return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(
+            v
+          );
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 10
+    },
+    email_verified_at: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    // Reset Password Schema
+    reset: {
+      token: {
+        type: String,
+        required: false,
+        length: 90,
+        expires: "15m"
+      }
+    }
+  },
+  { timestamps: true }
+);
