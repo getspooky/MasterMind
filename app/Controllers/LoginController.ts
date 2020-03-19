7dee8525546ae03d859bff812a7f565aecf5ff8e
yasser <spookyframework@gmail.com>
2020-03-19 19:40:00 +0100

    feat(Controller): Add Login Controller

--- app/Controllers/LoginController.ts
+++ app/Controllers/LoginController.ts
@@ -3,11 +3,17 @@
  *
  * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright andc license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { LoginInterface } from "../Interfaces/LoginInterface";
+import { SessionInterface } from "../Interfaces/SessionInterface";
+import { generateSessionKey } from "../Controllers/RegisterController";
+import { env } from "../../helpers";
+import User from "../Models/User";
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +51,28 @@ const index = function(req: Request, res: Response): void {
  * @param {Response} res
  * @returns {void}
  */
-const login = function(req: Request, res: Response): void {};
+const login = async function(
+  req: SessionInterface,
+  res: Response
+): Promise<void | TypeError> {
+  const { email, password }: LoginInterface = { ...req.body };
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  if (!validationResult(req).isEmpty)
+    return new TypeError("The given inputs was Invalid");
+  // Attempt to log the user into the application.
+  const attemptLogin = await User.findOne({ email });
+  if (
+    !attemptLogin &&
+    !(await User.comparePassword(password, attemptLogin.password))
+  )
+    return new TypeError("Account does not exists!");
+  // store session id
+  req.session.user_id_token = generateSessionKey(
+    email,
+    attemptLogin._id,
+    env("SESSION_GENERATOR")
+  );
+  return res.status(201).redirect(redirectTo);
+};
 
 export default { index, login };
