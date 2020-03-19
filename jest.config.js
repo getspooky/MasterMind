13c0204a044c3eb9645bd825fd5a2d28edbdb00e
yasser <spookyframework@gmail.com>
2020-03-19 14:34:30 +0100

    integrate jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the MasterMind project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+module.exports = {
+    roots: ['<rootDir>/app'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'json', 'node'],
+};
