4e2d4dcd432f6db7834850aa8242fa7131834d60
yasser <spookyframework@gmail.com>
2020-03-19 14:42:04 +0100

    update: change testRegex

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
-/*
- * This file is part of the MasterMind project.
- *
- * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 module.exports = {
-    roots: ['<rootDir>/app'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'json', 'node'],
+  roots: ["./"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/tests/)",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["/(dist|node_modules)/"]
 };
