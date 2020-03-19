dcda3472ab3f216e418f8e324a14a4dfe2b746f2
yasser <spookyframework@gmail.com>
2020-03-19 17:12:49 +0100

    feat(Middleware): Add Validator middleware

---
+++ app/Middlewares/Validator.ts
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the MasterMind project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { check, ValidationChain } from "express-validator";
+
+/**
+ * @type {string}
+ */
+type ServiceType = "Login" | "Register";
+
+/**
+ * @public
+ * @desc Get a validator for an incoming registration request.
+ * @function
+ * @name validator
+ * @param {ServiceType} service
+ * @returns {Array<ValidationChain>}
+ */
+const validator: Function = function(
+  service: ServiceType
+): Array<ValidationChain> {
+  return [
+    check("email")
+      .not()
+      .isEmpty()
+      .withMessage("Email is missing")
+      .isEmail()
+      .withMessage("Email is not valid"),
+    check("password")
+      .not()
+      .isEmpty()
+      .withMessage("Password is missing")
+      .isLength({
+        min: 10
+      }),
+    service === "Register"
+      ? check("username")
+          .not()
+          .isEmpty()
+          .withMessage("Username is missing")
+          .isLength({
+            min: 10
+          })
+          .withMessage("Username must have more than 10 characters")
+      : null
+  ];
+};
