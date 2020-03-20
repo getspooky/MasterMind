839fe54c7407e687510c3790b8461af647f7a8a8
yasser <spookyframework@gmail.com>
2020-03-20 19:10:16 +0100

    feat(Profile): Add Profile Controller

---
+++ app/Controllers/ProfileController.ts
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the MasterMind project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+
+/**
+ * @public
+ * @desc Display a listing of the resource.
+ * @function
+ * @name index
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {void}
+ */
+const index = async function(req: Request, res: Response): Promise<void> {
+  return res.status(200).render("Profile");
+};
+
+export default { index };
