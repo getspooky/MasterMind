4def03b02011e46e2f9fdfe45b9e3e0ad8b84a3b
yasser <spookyframework@gmail.com>
2020-03-20 18:03:41 +0100

    feat(Routes): Add Article routes

--- routes/web.ts
+++ routes/web.ts
@@ -13,7 +13,10 @@ import { compose as ComposeMiddleware } from "compose-middleware";
 import LoginController from "../app/Controllers/LoginController";
 import RegisterController from "../app/Controllers/RegisterController";
 import ArticleController from "../app/Controllers/ArticleController";
-import { Validator } from "../app/Middlewares/Validator";
+import ProfileController from "../app/Controllers/ArticleController";
+import LoginValidator from "../app/Validators/Login";
+import RegisterValidator from "../app/Validators/Register";
+import ArticleValidator from "../app/Validators/Article";
 import { Authenticated } from "../app/Middlewares/Authenticated";
 
 /*
@@ -35,15 +38,15 @@ Route.get("/", (req: Request, res: Response): void => res.render("Welcome"));
 
 Route.get("/login", LoginController.index);
 
-Route.post("/login", Validator("Login"), LoginController.login);
+Route.post("/login", LoginValidator(), LoginController.login);
 
 Route.get("/register", RegisterController.index);
 
-Route.post("/register", Validator("Register"), RegisterController.register);
+Route.post("/register", RegisterValidator(), RegisterController.register);
 
 // Routes that require authentication
 
-Route.get("/profile", Authenticated, (req, res) => res.render("Profile"));
+Route.get("/profile", Authenticated, ProfileController.index);
 
 Route.get("/articles", Authenticated, ArticleController.index);
 
@@ -51,6 +54,6 @@ Route.get("/article/create", Authenticated, ArticleController.create);
 
 Route.post(
   "/article",
-  ComposeMiddleware([Authenticated]),
+  ComposeMiddleware([Authenticated, ArticleValidator()]),
   ArticleController.store
 );
